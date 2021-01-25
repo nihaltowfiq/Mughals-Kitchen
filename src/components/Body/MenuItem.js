7ae1097e6380069b740471ff4903e7da2f66cd30
nihaltowfiq <nihaltowfiq@gmail.com>
2021-01-25 22:39:33 +0600

    designing menuItem

--- src/components/Body/MenuItem.js
+++ src/components/Body/MenuItem.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Card, CardBody, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
 const MenuItem = (props) => {
-  console.log(props.dish.name);
+  console.log(props.dish);
   return (
     <div>
-      <h1>{props.dish.name}</h1>
+      <Card className="mt-3">
+        <CardBody>
+          <CardImg
+            width="100%"
+            alt={props.dish.name}
+            src={props.dish.image}
+            style={{ opacity: "0.7" }}
+          ></CardImg>
+          <CardImgOverlay>
+            <CardTitle>
+              <h3>{props.dish.name}</h3>
+            </CardTitle>
+          </CardImgOverlay>
+        </CardBody>
+      </Card>
     </div>
   );
 };
