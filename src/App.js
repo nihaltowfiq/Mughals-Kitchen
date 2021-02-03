f69aca8a124063b3c93fa342271f456e682828fe
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-03 07:25:59 +0600

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import MainComponent from "./components/MainComponent";
 
 function App() {
   return (
     <div className="app">
-      <MainComponent />
+      <Router>
+        <MainComponent />
+      </Router>
     </div>
   );
 }
