ec2769116e773fb08173fd4e30f963a4c804941a
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-04 11:30:47 +0600

    footer added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-auto">
+            <p>Mughal's Kicthen &#169; Copyright 2021</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
