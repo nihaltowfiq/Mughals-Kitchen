f712dc4361230f8b25bd024c705ed55de78e78ed
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-05 09:26:24 +0600

    create redux-store and provided

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import MainComponent from "./components/MainComponent";
+import myStore from "./redux/store";
 
 function App() {
   return (
     <div className="app">
-      <Router>
-        <MainComponent />
-      </Router>
+      <Provider store={myStore}>
+        <Router>
+          <MainComponent />
+        </Router>
+      </Provider>
     </div>
   );
 }
