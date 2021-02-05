2138fecdd5f1e71b491aca6f0bbb317630fc927e
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-05 09:03:37 +0600

    dynamic title changed

--- src/components/Body/Home.js
+++ src/components/Body/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Home extends Component {
   render() {
-    document.title = "Home";
+    document.title = "Mughal's Kitchen - Home";
     return (
       <div>
         <h1>This is HOME</h1>
