a2d7da4a2f8de85aaba2c137adebe961655861ca
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-06 11:08:33 +0600

    sample reducer-action practice

--- src/components/Body/Home.js
+++ src/components/Body/Home.js
@@ -2,19 +2,26 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 const mapStateToProps = (state) => {
-  console.log("mapStateToProps: ", state);
   return {
     dishes: state.dishes,
+    sample: state.sample,
   };
 };
 
 class Home extends Component {
   render() {
-    console.log(this.props);
+    this.props.dispatch({
+      type: "TEST",
+      str: "Bohubrihi",
+    });
     document.title = "Mughal's Kitchen - Home";
     return (
       <div>
         <h1>This is HOME</h1>
+        {this.props.sample}
+        {this.props.dishes.map((dish) => (
+          <h1 key={dish.id}>{dish.name}</h1>
+        ))}
       </div>
     );
   }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,8 +4,16 @@ import COMMENTS from "../data/comments";
 const initialState = {
   dishes: DISHES,
   comments: COMMENTS,
+  sample: "Hello World",
 };
 
 export const Reducer = (state = initialState, action) => {
+  console.log(action.type);
+  if (action.type === "TEST") {
+    return {
+      ...state,
+      sample: action.str,
+    };
+  }
   return state;
 };
