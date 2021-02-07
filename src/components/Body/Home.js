d870fe3f4bb61bd621a36d76dd70d1bae7cd78bd
nihaltowfiq <nihaltowfiq@gmail.com>
2021-02-07 11:09:49 +0600

    added Loading.js

--- src/components/Body/Home.js
+++ src/components/Body/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Loading from "./Loading";
 class Home extends Component {
   render() {
     document.title = "Mughal's Kitchen - Home";
     return (
       <div>
         <h1>This is HOME</h1>
+        <Loading />
       </div>
     );
   }
---
+++ src/components/Body/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="text-center p-5">
+      <span className="fa fa-spinner text-primary fa-4x fa-fw fa-pulse"></span>
+    </div>
+  );
+};
+
+export default Loading;
